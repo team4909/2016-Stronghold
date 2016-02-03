dfe1074c66f015f997228d28b7ab30ae680b2f46
FRC4909 <billericabionics@gmail.com>
2016-02-02 19:11:19 -0500

    Chris finished basic drivetrain control

--- src/org/usfirst/frc4909/Bionics2016/commands/Drive.java
+++ src/org/usfirst/frc4909/Bionics2016/commands/Drive.java
@@ -42,6 +42,7 @@ public class Drive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.drivetrain.moveRobot(Robot.oi.getLeft(), Robot.oi.getRight());
     }
 
     // Make this return true when this Command no longer needs to run execute()
