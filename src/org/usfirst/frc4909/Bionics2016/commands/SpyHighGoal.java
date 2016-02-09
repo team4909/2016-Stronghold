4c7be7ec4b531ab05b6d137ac077c5143ef95ecd
FRC4909 <billericabionics@gmail.com>
2016-02-08 22:18:48 -0500

    Chris added blank autonomous commands

---
+++ src/org/usfirst/frc4909/Bionics2016/commands/SpyHighGoal.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc4909.Bionics2016.commands;
+
+import org.usfirst.frc4909.Bionics2016.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SpyHighGoal extends Command {
+
+	public SpyHighGoal() {
+		 requires(Robot.drivetrain);
+	        requires(Robot.shooter);
+	        requires(Robot.pivot);
+	        requires(Robot.feeder);
+	}
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
