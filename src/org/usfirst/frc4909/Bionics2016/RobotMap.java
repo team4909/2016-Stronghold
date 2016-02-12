508acc03b5b7c8c578030c7678fa419ab23707f5
FRC4909 <billericabionics@gmail.com>
2016-02-12 18:39:40 -0500

    Chris tested with pivot

--- src/org/usfirst/frc4909/Bionics2016/RobotMap.java
+++ src/org/usfirst/frc4909/Bionics2016/RobotMap.java
@@ -182,9 +182,9 @@ public class RobotMap {
         pivotBottomSwitch = new DigitalInput(2);
         LiveWindow.addSensor("Feeder", "feedSwitch", feederfeedSwitch);
         
-        pivotpivotEncoder = new Encoder(3, 4, true, EncodingType.k4X);
+        pivotpivotEncoder = new Encoder(3, 4, false, EncodingType.k4X);
         LiveWindow.addSensor("Pivot", "pivotEncoder", pivotpivotEncoder);
-        pivotpivotEncoder.setDistancePerPulse(360.0/8192);
+        pivotpivotEncoder.setDistancePerPulse(360.0/2048);
         pivotpivotEncoder.setPIDSourceType(PIDSourceType.kDisplacement);
         
         
